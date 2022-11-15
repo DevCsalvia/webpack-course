d07ac7d1bb6a3e99d696799a01d2ca4151e78601
devcsalvia <vip_heromen_vip@mail.ru>
2022-11-15 07:19:58 +0300

    9. CSS code splitting(remove flicker, css preloaded) MiniCssExtractPlugin

---
+++ 9. Split_CSS/webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const {BundleAnalyzerPlugin} = require("webpack-bundle-analyzer")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    courses: "./src/pages/courses.js"
+  },
+  output: {
+    // if contenthash changes bundle will be downloaded again
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, "dist"),
+    // Clean dist on every build
+    clean: true
+  },
+  devServer: {
+    static: "./dist",
+    client:{
+      overlay: false
+    }
+  },
+  module:{
+    rules:[
+      {
+        test: /\.css$/,
+        // Style-loader injected css styles into bundle
+        // MiniCssExtractPlugin will create css files in dist folder
+        use: [MiniCssExtractPlugin.loader,
+          {loader: "css-loader"}
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/,
+        // Style-loader injected css styles into bundle
+        // MiniCssExtractPlugin will create css files in dist folder
+        use: [
+          MiniCssExtractPlugin.loader,
+          {loader: "css-loader"},
+          {loader: "sass-loader"}
+        ]
+      }
+    ]
+  },
+  plugins:[
+    // To preserve index.html template with entry(chunk) 'index: "./src/index.js"' injected and bundle into file with filename: "index.html"
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+      chunks: ["index"],
+      inject: true,
+      filename: "index.html"
+    }),
+    // To preserve courses.html template with entry(chunk) 'courses: "./src/pages/courses.js"' injected and bundle into file with filename: "courses.html"
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/pages/courses.html"),
+      chunks: ["courses"],
+      inject: true,
+      filename: "courses.html"
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets/images/*"),
+          to: path.resolve(__dirname, "dist"),
+          context: "src"
+        }
+      ]
+    }),
+    // new BundleAnalyzerPlugin({}),
+      new MiniCssExtractPlugin({})
+  ],
+  // Shared packages move to one chunk and loaded separately then caching of browsers do not reload them
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  }
+}

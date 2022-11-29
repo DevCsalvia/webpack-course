2191d018161ca442598d2c7c275d2f6fcfe4c529
devcsalvia <vip_heromen_vip@mail.ru>
2022-11-29 06:31:05 +0300

    11. Fixing routes(add bootstrap file)

--- 11 MF_project_base/MovieApp/webpack.config.js
+++ 11 MF_project_base/MovieApp/webpack.config.js
@@ -1,7 +1,7 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
-const { ModuleFederationPlugin } = require("webpack").container;
+const {ModuleFederationPlugin} = require("webpack").container;
 
 module.exports = {
   mode: "development",
@@ -15,26 +15,9 @@ module.exports = {
       directory: path.resolve(__dirname, "dist"),
     },
     open: true,
-    port: 3000,
+    port: 9000,
+    historyApiFallback: true,
   },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "home",
-      filename: "remoteEntry.js",
-      remotes: {
-        components: "components@http://localhost:3002/remoteEntry.js"
-      },
-      exposes: {
-        "./HomePage": "./src/components/HomeContent/HomeContent.jsx",
-      },
-      shared: ["react", "react-dom"]
-    }),
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-    }),
-  ],
   module: {
     rules: [
       {
@@ -71,11 +54,34 @@ module.exports = {
           "sass-loader",
         ],
       },
+      {
+        test: /\.(png|jpeg|gif|jpg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.webp$/i,
+        use: ["file-loader","webp-loader"],
+      },
     ],
   },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "movieapp",
+      filename: "remoteEntry.js",
+      remotes: {
+        homepage: "home@http://localhost:3000/remoteEntry.js"
+      },
+      shared: ["react", "react-dom"]
+    }),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
   optimization: {
     splitChunks: {
-      chunks: "async",
+      chunks: "all",
     },
   },
 };
